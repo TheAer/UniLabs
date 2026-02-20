59f9803d185bdf57351f450caca606139bde4efa
TheAer <max.bykch123@gmail.com>
2026-02-20 12:48:01 +0200

    discrete math lab complete

--- Discrete Mathematics/Lab1/main.cpp
+++ Discrete Mathematics/Lab1/main.cpp
@@ -5,4 +5,78 @@ using namespace std;
 
 int main() {
     bool a, b, c;
+
+    cout << "| a | b | NOT (a) | AND (a,b) | OR (a,b) | IMP (a,b) | EQU (a,b) | ""XOR (a,b) | " << endl;
+    cout << "|============================================================================|" << endl;
+    a = true;
+    b = true;
+
+    cout <<"| "<<a<<" | "<<b<<" |    "<<NOT(a)<< "    |     "<< AND (a, b)<<"     |     "<< OR(a,b)<<"    |     "<< IMP (a,b)<<"     |     "<<EQU(a,b)<<"     |     "<< XOR(a,b)<<"     | "<<endl;
+
+    a = true;
+    b = false;
+
+    cout <<"| "<<a<<" | "<<b<<" |    "<<NOT(a)<< "    |     "<< AND (a, b)<<"     |     "<< OR(a,b)<<"    |     "<< IMP (a,b)<<"     |     "<<EQU(a,b)<<"     |     "<< XOR(a,b)<<"     | "<<endl;
+
+    a = false;
+    b = true;
+
+    cout <<"| "<<a<<" | "<<b<<" |    "<<NOT(a)<< "    |     "<< AND (a, b)<<"     |     "<< OR(a,b)<<"    |     "<< IMP (a,b)<<"     |     "<<EQU(a,b)<<"     |     "<< XOR(a,b)<<"     | "<<endl;
+
+    a = false;
+    b = false;
+
+    cout <<"| "<<a<<" | "<<b<<" |    "<<NOT(a)<< "    |     "<< AND (a, b)<<"     |     "<< OR(a,b)<<"    |     "<< IMP (a,b)<<"     |     "<<EQU(a,b)<<"     |     "<< XOR(a,b)<<"     | "<<endl;
+    cout << "|============================================================================|" << endl;
+
+    cout << endl;
+
+    cout << endl;
+
+    cout << "| a | b | с | FUNC3 (a, b, c)| " << endl;
+    cout << "|============================|" << endl;
+    
+    a = true;
+    b = true;
+    c = true;
+    cout <<"| "<<a<<" | "<<b<<" | "<<c<<" |        "<< FUNC3 (a, b, c)<<"       | "<<endl;
+
+    a = true;
+    b = true;
+    c = false;
+    cout <<"| "<<a<<" | "<<b<<" | "<<c<<" |        "<< FUNC3 (a, b, c)<<"       | "<<endl;
+
+    a = true;
+    b = false;
+    c = true;
+    cout <<"| "<<a<<" | "<<b<<" | "<<c<<" |        "<< FUNC3 (a, b, c)<<"       | "<<endl;
+
+    a = true;
+    b = false;
+    c = false;
+    cout <<"| "<<a<<" | "<<b<<" | "<<c<<" |        "<< FUNC3 (a, b, c)<<"       | "<<endl;
+
+    a = false;
+    b = true;
+    c = true;
+    cout <<"| "<<a<<" | "<<b<<" | "<<c<<" |        "<< FUNC3 (a, b, c)<<"       | "<<endl;
+
+    a = false;
+    b = true;
+    c = false;
+    cout <<"| "<<a<<" | "<<b<<" | "<<c<<" |        "<< FUNC3 (a, b, c)<<"       | "<<endl;
+
+    a = false;
+    b = false;
+    c = true;
+    cout <<"| "<<a<<" | "<<b<<" | "<<c<<" |        "<< FUNC3 (a, b, c)<<"       | "<<endl;
+
+    a = false;
+    b = false;
+    c = false;
+    cout <<"| "<<a<<" | "<<b<<" | "<<c<<" |        "<< FUNC3 (a, b, c)<<"       | "<<endl;
+
+    cout << "|============================|" << endl;
+    
+    return 0;
 }
